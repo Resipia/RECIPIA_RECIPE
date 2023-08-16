acbc35a286c2498ac0d85483988cde2a222f3487
wlsdks <wlsdks12@naver.com>
2023-08-16 20:40:17 +0900

    first init

---
+++ src/main/java/com/recipia/recipe/RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.recipia.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApplication.class, args);
+	}
+
+}

df564b0a18f7464152a03ae311e156c067e7fd6d
yijoon009 <yijoon009@gmail.com>
2023-09-11 22:39:20 +0900

    RCP-11 fix: 불필요한 파일 삭제 및 임시 테스트 코드

--- src/main/java/com/recipia/recipe/controller/RecipeController.java
+++ src/main/java/com/recipia/recipe/controller/RecipeController.java
@@ -1,9 +1,16 @@
 package com.recipia.recipe.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/recipe")
 public class RecipeController {
+
+    @GetMapping("hte")
+    public String kafkatest () {
+        return null;
+    }
+
 }

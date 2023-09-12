7dbd795609e186bb262a6fc151ac6dc14e5944e6
yijoon009 <yijoon009@gmail.com>
2023-09-12 21:24:51 +0900

    RCP-13 feat: zipkin 설정 추가

--- src/main/java/com/recipia/recipe/controller/RecipeController.java
+++ src/main/java/com/recipia/recipe/controller/RecipeController.java
@@ -1,15 +1,18 @@
 package com.recipia.recipe.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/recipe")
 public class RecipeController {
 
     @GetMapping("hte")
     public String kafkatest () {
+        log.info("recipe-controller");
         return null;
     }
 

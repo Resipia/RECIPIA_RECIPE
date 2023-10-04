b4f5d0722d684e4ca29f93c89c50e70c9ffe5caf
wlsdks <wlsdks12@naver.com>
2023-10-04 17:47:40 +0900

    RCP-16 feat: Kafka를 통해 Consumer가 동작하는 로직을 구현했다.
    1. KafkaConsumerConfig 클래스를 작성해서 AcknowledgingMessageListener를 사용할때 ack를 수동으로 컨트롤 할 수 있도록 설정함
    2. Recipe서버에서 member정보를 받아서 parsing할 수 있도록 필요정보만 필드로 가지는 dto를 작성함
    3. 서비스에서 kafka이벤트를 listen하여 동작하는 로직을 작성함

--- src/main/java/com/recipia/recipe/controller/RecipeController.java
+++ src/main/java/com/recipia/recipe/controller/RecipeController.java
@@ -4,6 +4,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- src/main/java/com/recipia/recipe/domain/repository/RecipeRepository.java
+++ src/main/java/com/recipia/recipe/domain/repository/RecipeRepository.java
@@ -1,9 +1,13 @@
 package com.recipia.recipe.domain.repository;
 
 import com.recipia.recipe.domain.Recipe;
+import com.recipia.recipe.dto.member.MemberDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    Optional<Recipe> updateRecipeByMemberIdAndNickname(Long memberId, String nickname);
 }

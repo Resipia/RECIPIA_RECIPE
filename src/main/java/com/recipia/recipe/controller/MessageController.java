c76328ab3f77b81d60e3fe3786057d1a2ca95e59
wlsdks <wlsdks12@naver.com>
2023-11-09 00:12:12 +0900

    RCP-26 feat: SNS 메시지 발행을 테스트할 controller, service 코드 작성

---
+++ src/main/java/com/recipia/recipe/controller/MessageController.java
@@ -0,0 +1,31 @@
+package com.recipia.recipe.controller;
+
+import com.recipia.recipe.aws.SnsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import software.amazon.awssdk.services.sns.model.PublishResponse;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/message")
+public class MessageController {
+
+    private final SnsService snsService;
+
+    @PostMapping("/publish")
+    public ResponseEntity<String> publishMessage(@RequestBody Map<String, Object> messageMap) {
+        // 1. SnsService를 사용해서 메시지 발행
+        PublishResponse response =  snsService.publishNicknameToTopic(messageMap);
+
+        // 2. 발행 결과를 HTTP 응답으로 반환
+        return ResponseEntity.ok().body(response.messageId());
+
+    }
+
+}

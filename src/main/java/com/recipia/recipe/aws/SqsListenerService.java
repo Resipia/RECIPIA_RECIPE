d9092f6c43d6187c5cb3448cdf8f759833bdf66e
wlsdks <wlsdks12@naver.com>
2023-11-09 00:11:40 +0900

    RCP-26 feat: sqs 메시지를 받아줄 테스트 서비스 코드 작성

---
+++ src/main/java/com/recipia/recipe/aws/SqsListenerService.java
@@ -0,0 +1,40 @@
+package com.recipia.recipe.aws;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class SqsListenerService {
+
+    private final ObjectMapper objectMapper;
+
+    @SqsListener(value = "${spring.cloud.aws.sqs.nickname-sqs-name}")
+    public void receiveMessage(String messageJson) {
+
+        try {
+            JsonNode messageNode = objectMapper.readTree(messageJson);
+            String topicArn = messageNode.get("TopicArn").asText();
+            String messageContent = messageNode.get("Message").asText();
+
+            // Assuming the "Message" is also a JSON string, we parse it to print as JSON object
+            JsonNode message = objectMapper.readTree(messageContent);
+
+            
+            log.info("Topic ARN: {}", topicArn);
+            log.info("Message:  {}", message.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Error parsing message JSON", e);
+        }
+
+    }
+
+}

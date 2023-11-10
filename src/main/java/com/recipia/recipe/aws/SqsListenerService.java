84c0689f3dcb3397dab4c369213459731045dadc
wlsdks <wlsdks12@naver.com>
2023-11-10 13:50:51 +0900

    SubSegment에서 Segment가 없다는 오류가 나와서 Segment 클래스를 추가

--- src/main/java/com/recipia/recipe/aws/SqsListenerService.java
+++ src/main/java/com/recipia/recipe/aws/SqsListenerService.java
@@ -1,6 +1,7 @@
 package com.recipia.recipe.aws;
 
 import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.entities.Segment;
 import com.amazonaws.xray.entities.Subsegment;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -21,26 +22,33 @@ public class SqsListenerService {
     @SqsListener(value = "${spring.cloud.aws.sqs.nickname-sqs-name}")
     public void receiveMessage(String messageJson) {
 
-        Subsegment subsegment = AWSXRay.beginSubsegment("publishNicknameToTopic");
+        // 부모 세그먼트 생성
+        Segment segment = AWSXRay.beginSegment("publishNicknameToTopicSegment");
 
         try {
-            JsonNode messageNode = objectMapper.readTree(messageJson);
-            String topicArn = messageNode.get("TopicArn").asText();
-            String messageContent = messageNode.get("Message").asText();
+            // 여기서 서브세그먼트 생성
+            Subsegment subsegment = AWSXRay.beginSubsegment("publishNicknameToTopic");
 
-            // Assuming the "Message" is also a JSON string, we parse it to print as JSON object
-            JsonNode message = objectMapper.readTree(messageContent);
+            try{
+                JsonNode messageNode = objectMapper.readTree(messageJson);
+                String topicArn = messageNode.get("TopicArn").asText();
+                String messageContent = messageNode.get("Message").asText();
 
-            
-            log.info("Topic ARN: {}", topicArn);
-            log.info("Message:  {}", message.toString());
-        } catch (IOException e) {
-            subsegment.addException(e);
-            throw new RuntimeException("Error parsing message JSON", e);
+                // Assuming the "Message" is also a JSON string, we parse it to print as JSON object
+                JsonNode message = objectMapper.readTree(messageContent);
+
+
+                log.info("Topic ARN: {}", topicArn);
+                log.info("Message:  {}", message.toString());
+            } catch (IOException e) {
+                subsegment.addException(e);
+                throw new RuntimeException("Error parsing message JSON", e);
+            } finally {
+                AWSXRay.endSubsegment();
+            }
         } finally {
-            AWSXRay.endSubsegment();
+            AWSXRay.endSegment();
         }
-
     }
 
 }

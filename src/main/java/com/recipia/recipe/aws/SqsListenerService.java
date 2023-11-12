63fd4be4fd5d8c807bf3a888105ffeaf7d0a0a2b
yijoon009 <yijoon009@gmail.com>
2023-11-12 16:16:16 +0900

    xray 제거

--- src/main/java/com/recipia/recipe/aws/SqsListenerService.java
+++ src/main/java/com/recipia/recipe/aws/SqsListenerService.java
@@ -1,8 +1,6 @@
 package com.recipia.recipe.aws;
 
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.entities.Segment;
-import com.amazonaws.xray.entities.Subsegment;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.awspring.cloud.sqs.annotation.SqsListener;
@@ -20,35 +18,17 @@ public class SqsListenerService {
     private final ObjectMapper objectMapper;
 
     @SqsListener(value = "${spring.cloud.aws.sqs.nickname-sqs-name}")
-    public void receiveMessage(String messageJson) {
+    public void receiveMessage(String messageJson) throws JsonProcessingException {
+        JsonNode messageNode = objectMapper.readTree(messageJson);
+        String topicArn = messageNode.get("TopicArn").asText();
+        String messageContent = messageNode.get("Message").asText();
 
-        // 부모 세그먼트 생성
-        Segment segment = AWSXRay.beginSegment("publishNicknameToTopicSegment");
-
-        try {
-            // 여기서 서브세그먼트 생성
-            Subsegment subsegment = AWSXRay.beginSubsegment("publishNicknameToTopic");
-
-            try{
-                JsonNode messageNode = objectMapper.readTree(messageJson);
-                String topicArn = messageNode.get("TopicArn").asText();
-                String messageContent = messageNode.get("Message").asText();
-
-                // Assuming the "Message" is also a JSON string, we parse it to print as JSON object
-                JsonNode message = objectMapper.readTree(messageContent);
+        // Assuming the "Message" is also a JSON string, we parse it to print as JSON object
+        JsonNode message = objectMapper.readTree(messageContent);
 
 
-                log.info("Topic ARN: {}", topicArn);
-                log.info("Message:  {}", message.toString());
-            } catch (IOException e) {
-                subsegment.addException(e);
-                throw new RuntimeException("Error parsing message JSON", e);
-            } finally {
-                AWSXRay.endSubsegment();
-            }
-        } finally {
-            AWSXRay.endSegment();
-        }
+        log.info("Topic ARN: {}", topicArn);
+        log.info("Message:  {}", message.toString());
     }
 
 }

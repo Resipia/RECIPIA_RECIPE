64785d1e3240b56c17e4f7d1ef562730612cd233
wlsdks <wlsdks12@naver.com>
2023-11-19 23:34:46 +0900

    RCP-59 refactor: zipkin 추적에 에러관련 로그가 남도록 코드추가, ThreadLocal코드는 필요없어져서 제거함

--- src/main/java/com/recipia/recipe/event/sqseventlistener/RequestFeignListener.java
+++ src/main/java/com/recipia/recipe/event/sqseventlistener/RequestFeignListener.java
@@ -1,12 +1,14 @@
 package com.recipia.recipe.event.sqseventlistener;
 
 
+import brave.Span;
+import brave.Tracer;
 import com.recipia.recipe.aws.AwsSqsListenerService;
 import com.recipia.recipe.domain.Recipe;
-import com.recipia.recipe.repository.RecipeRepository;
 import com.recipia.recipe.event.springevent.NicknameChangeEvent;
 import com.recipia.recipe.feign.MemberFeignClient;
 import com.recipia.recipe.feign.dto.NicknameDto;
+import com.recipia.recipe.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -22,8 +24,7 @@ public class RequestFeignListener {
 
     private final MemberFeignClient memberFeignClient;
     private final RecipeRepository recipeRepository;
-
-    private final AwsSqsListenerService awsSqsListenerService;
+    private final Tracer tracer;
 
     /**
      * Feign 클라이언트로 Member서버에 변경된 닉네임을 요청하는 리스너
@@ -31,18 +32,29 @@ public class RequestFeignListener {
     @Transactional
     @EventListener
     public void requestMemberChangedNickname(NicknameChangeEvent event) {
+        Span feignRequestSpan = tracer.nextSpan().name("[RECIPE] /feign/member/getNickname request").start();
         Long memberId = event.memberId();
-        NicknameDto nicknameDto = memberFeignClient.getNickname(memberId);
 
-        List<Recipe> recipeList = recipeRepository.findRecipeByMemberIdAndDelYn(memberId, "N");
-        // 닉네임을 변경한 사용자가 작성한 레시피들의 nickname을 변경한다.
-        if (!recipeList.isEmpty()) {
-            recipeList.forEach(recipe -> {
-                recipe.changeNickname(nicknameDto.nickname());
-            });
+        NicknameDto nicknameDto;
+        try (Tracer.SpanInScope ws = tracer.withSpanInScope(feignRequestSpan)) {
+            nicknameDto = memberFeignClient.getNickname(memberId);
+            // Feign 요청 후 응답 처리
+            if (nicknameDto != null) {
+                // NicknameDto 처리 로직
+                List<Recipe> recipeList = recipeRepository.findRecipeByMemberIdAndDelYn(memberId, "N");
+                if (!recipeList.isEmpty()) {
+                    recipeList.forEach(recipe -> recipe.changeNickname(nicknameDto.nickname()));
+                }
+            }
+        } catch (Exception e) {
+            // 에러 태그 추가
+            feignRequestSpan.tag("error", e.toString());
+
+            // 에러 로깅
+            log.error("Feign request error: ", e);
+        } finally {
+            feignRequestSpan.finish();
         }
-        // Span을 닫는 메소드 호출
-        awsSqsListenerService.closeCurrentSpan();
     }
 
 }

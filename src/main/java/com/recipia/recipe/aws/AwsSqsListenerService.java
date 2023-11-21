47517d1a1ab0b5ad6f01fb90c4adb7beaa6aae0c
wlsdks <wlsdks12@naver.com>
2023-11-21 17:25:33 +0900

    RCP-160 refactor: 기존의 코드에서 예외처리를 추가하고 DTO를 사용하여 Json을 제거하고 최적화를 진행함

--- src/main/java/com/recipia/recipe/aws/AwsSqsListenerService.java
+++ src/main/java/com/recipia/recipe/aws/AwsSqsListenerService.java
@@ -3,6 +3,8 @@ package com.recipia.recipe.aws;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.recipia.recipe.dto.SnsInformationDto;
+import com.recipia.recipe.dto.SnsMessageDto;
 import com.recipia.recipe.event.springevent.NicknameChangeEvent;
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.RequiredArgsConstructor;
@@ -24,28 +26,34 @@ public class AwsSqsListenerService {
     private final Tracer tracer;
 
 
+    /**
+     * 아래의 Sqs리스너는 멤버 서버로 FeignClient 요청을 하는 스프링 이벤트를 발행한다.
+     *
+     * @apiNote
+     * 만약 traceId가 들어있지 않은 메시지를 받았다면 여기서는 error에 대한 로깅만 하고 메서드를 바로 return시킨다.
+     * SQS메시지 안에 traceId가 없으면 이 메시지를 동시에 받아 동작하는 멤버 서버의 SQS리스너에서는 이벤트 발행여부를 false로 둘 것이다.
+     * 그럼 배치서버에서는 이 메시지의 발행여부가 false여서 다시 이벤트를 발행한다. (이때 traceId를 만들어서 넣어준다.)
+     */
     @SqsListener(value = "${spring.cloud.aws.sqs.nickname-sqs-name}")
     public void receiveMessage(String messageJson) throws JsonProcessingException {
 
-        JsonNode messageNode = objectMapper.readTree(messageJson);
-        String messageId = messageNode.get("MessageId").asText();
-        String messageContent = messageNode.get("Message").asText();
+        SnsInformationDto snsInformationDto = objectMapper.readValue(messageJson, SnsInformationDto.class);
+        SnsMessageDto snsMessageDto = objectMapper.readValue(snsInformationDto.Message(), SnsMessageDto.class);
 
-        log.info("[RECIPE] Received message from SQS with messageId: {}", messageId);
+        if (snsMessageDto.traceId() == null) {
+//            log.error("No traceId found in the message. memberId: {}, skipping processing.", snsMessageDto.memberId());
+            return;
+        }
 
-        JsonNode message = objectMapper.readTree(messageContent);
-        String traceId = extractTraceIdFromMessage(message);
-
-        // 이전 서버에서 보낸 traceId를 사용하여 새로운 TraceContext를 생성한다.
-        TraceContext context = buildTraceContext(traceId);
-
-        // 이 Span은 이전 서버에서 생성된 traceId를 사용하고, 새로운 spanId를 갖게 된다.
+        // 이전 서버에서 보낸 traceId를 사용하여 새로운 TraceContext를 생성
+        TraceContext context = buildTraceContext(snsMessageDto.traceId());
         Span span = tracer.nextSpan(TraceContextOrSamplingFlags.create(context))
-                .name("[RECIPE] nickname-change SQS Received") // Span 이름 지정
+                .name("[RECIPE] nickname-change SQS Received")
                 .start();
 
+        // 추출된 memberId로 이벤트 발행 및 로깅
         try (Tracer.SpanInScope ws = tracer.withSpanInScope(span)) {
-            processNicknameMessage(message);
+            eventPublisher.publishEvent(new NicknameChangeEvent(snsMessageDto.memberId()));
         } catch (Exception e) {
             span.tag("error", e.toString());
             log.error("Error processing SQS message: ", e);
@@ -55,17 +63,14 @@ public class AwsSqsListenerService {
 
     }
 
-    private String extractTraceIdFromMessage(JsonNode message) {
-        // 메시지로부터 traceId를 추출합니다.
-        return message.get("traceId").asText();
-    }
-
+    /**
+     * [Extract Method] - TraceContext 생성
+     * 멤버 서버에서 받은 traceId를 TraceContext안에 세팅해 준다.
+     */
     private TraceContext buildTraceContext(String traceId) {
-        // 여기서 contextBuilder를 만들고 이 builder안에 멤버 서버에서 받은 traceId를 세팅해 준다.
         TraceContext.Builder contextBuilder = TraceContext.newBuilder();
 
-        // traceId의 길이에 따라 처리
-        // 32자리인 경우 128비트 traceId로 처리하고, 그렇지 않은 경우 64비트 traceId로 처리
+        // traceId의 길이가 32자리인 경우 128비트 traceId로 처리하고, 그렇지 않은 경우 64비트 traceId로 처리
         if (traceId.length() == 32) {
             long traceIdHigh = Long.parseUnsignedLong(traceId.substring(0, 16), 16);
             long traceIdLow = Long.parseUnsignedLong(traceId.substring(16), 16);
@@ -75,23 +80,9 @@ public class AwsSqsListenerService {
             contextBuilder.traceId(traceIdLow);
         }
 
-        // 새로운 Span ID를 생성
+        // 새로운 Span ID를 생성 하고 TraceContext 객체를 빌드해서 반환한다.
         contextBuilder.spanId(tracer.nextSpan().context().spanId());
-
-        // TraceContext 객체를 빌드하고 반환
         return contextBuilder.build();
     }
 
-    private void processNicknameMessage(JsonNode message) throws JsonProcessingException {
-        // 'message' 필드 내의 JSON 문자열을 추출
-        String messageContent = message.get("message").asText();
-
-        // 추출된 JSON 문자열을 파싱하여 memberId를 얻음
-        JsonNode innerMessageNode = objectMapper.readTree(messageContent);
-        Long memberId = Long.valueOf(innerMessageNode.get("memberId").asText());
-
-        // 추출된 memberId로 이벤트 발행 및 로깅
-        eventPublisher.publishEvent(new NicknameChangeEvent(memberId));
-        log.info("Processed NicknameChangeEvent for memberId: {}", memberId);
-    }
 }

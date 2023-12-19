6d5ab8ef135891e4e69f9b5bb16e4fdaba963718
wlsdks <wlsdks12@naver.com>
2023-12-20 00:30:26 +0900

    RCP-220 refactor: 완성하지 못했던 comment 엔티티 완성

--- src/main/java/com/recipia/recipe/hexagonal/adapter/out/persistence/Comment.java
+++ src/main/java/com/recipia/recipe/hexagonal/adapter/out/persistence/Comment.java
@@ -1,19 +1,64 @@
 package com.recipia.recipe.hexagonal.adapter.out.persistence;
 
+import com.recipia.recipe.hexagonal.adapter.out.persistence.auditingfield.UpdateDateTimeForEntity;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
+
+import java.util.Objects;
 
 @ToString(callSuper = true)
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Comment {
+public class Comment extends UpdateDateTimeForEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id", nullable = false)
+    @Column(name = "comment_id", nullable = false)
+    private Long id;
+
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private RecipeEntity recipe;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    private Long memberId;
+
+    @JoinColumn(name = "comment_text", nullable = false)
+    private String commentText;
+
+    @JoinColumn(name = "del_yn", nullable = false)
+    private String delYn;
+
+    @Builder
+    private Comment(Long id, RecipeEntity recipe, Long memberId, String commentText, String delYn) {
+        this.id = id;
+        this.recipe = recipe;
+        this.memberId = memberId;
+        this.commentText = commentText;
+        this.delYn = delYn;
+    }
+
+    public static Comment of(Long id, RecipeEntity recipe, Long memberId, String commentText, String delYn) {
+        return new Comment(id, recipe, memberId, commentText, delYn);
+    }
+
+    public static Comment of(RecipeEntity recipe, Long memberId, String commentText, String delYn) {
+        return new Comment(null, recipe, memberId, commentText, delYn);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Comment comment)) return false;
+        return this.id != null && Objects.equals(getId(), comment.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
 
 }

02602547f5694c0f51398539a670dbd4ce3ce342
wlsdks <wlsdks12@naver.com>
2023-12-23 16:25:21 +0900

    RCP-227 test: 레시피 생성 이벤트를 구독하는 리스너 코드 테스트 진행

--- src/test/java/com/recipia/recipe/config/TestZipkinConfig.java
+++ src/test/java/com/recipia/recipe/config/TestZipkinConfig.java
@@ -2,13 +2,13 @@ package com.recipia.recipe.config;
 
 import brave.Tracer;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("test")
-public class TestZipkinConfig {
+@TestConfiguration
+public  class TestZipkinConfig {
 
     @Bean
     public Tracer tracer() {

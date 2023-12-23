464056d4db5b7929693818a3687dce3e9598e723
wlsdks <wlsdks12@naver.com>
2023-12-23 15:37:54 +0900

    RCP-227 refactor: 몽고 document 객체에 기능 추가

--- src/main/java/com/recipia/recipe/adapter/out/persistence/document/HashtagDocument.java
+++ src/main/java/com/recipia/recipe/adapter/out/persistence/document/HashtagDocument.java
@@ -1,11 +1,17 @@
 package com.recipia.recipe.adapter.out.persistence.document;
 
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "hashtag")
 public class HashtagDocument {
 
     @Id

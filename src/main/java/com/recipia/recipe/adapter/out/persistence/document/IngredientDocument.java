f6c82dc7b4448e8daa1e853432c3aaceb0144d43
wlsdks <wlsdks12@naver.com>
2023-12-26 16:10:07 +0900

    RCP-248 refactor: 테스트 환경을 수정함

--- src/main/java/com/recipia/recipe/adapter/out/persistence/document/IngredientDocument.java
+++ src/main/java/com/recipia/recipe/adapter/out/persistence/document/IngredientDocument.java
@@ -11,8 +11,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-//@Document(collection = "ingredient")
-@Document(collection = "test")
+@Document(collection = "ingredient")
 public class IngredientDocument {
 
     @Id

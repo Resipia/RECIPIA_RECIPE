9149cb5454877dbc68016472219c94b8e3c29a30
wlsdks <wlsdks12@naver.com>
2023-12-31 22:42:26 +0900

    [RCP-254] :white_check_mark: 영양소 저장하는 어댑터 메서드 테스트 작성

--- src/test/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeAdapterTest.java
+++ src/test/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeAdapterTest.java
@@ -2,8 +2,10 @@ package com.recipia.recipe.adapter.out.persistenceAdapter;
 
 import com.recipia.recipe.adapter.out.feign.dto.NicknameDto;
 import com.recipia.recipe.adapter.out.persistence.document.IngredientDocument;
+import com.recipia.recipe.adapter.out.persistence.entity.NutritionalInfoEntity;
 import com.recipia.recipe.adapter.out.persistence.entity.RecipeEntity;
 import com.recipia.recipe.config.TotalTestSupport;
+import com.recipia.recipe.domain.NutritionalInfo;
 import com.recipia.recipe.domain.Recipe;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -20,6 +22,9 @@ import reactor.test.StepVerifier;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("[통합] 레시피 Adapter 테스트")
 class RecipeAdapterTest extends TotalTestSupport {
@@ -29,6 +34,9 @@ class RecipeAdapterTest extends TotalTestSupport {
 
     @Autowired
     private RecipeRepository recipeRepository;
+
+    @Autowired
+    private NutritionalInfoRepository nutritionalInfoRepository;
 
 //    @MockBean
 //    private ReactiveMongoTemplate mongoTemplate;
@@ -40,17 +48,16 @@ class RecipeAdapterTest extends TotalTestSupport {
     public void updateRecipesNicknames() {
 
         //given
-        Long memberId = 1L;
-        String changedNickname = "ChangedNickname";
-        NicknameDto mockDto = NicknameDto.of(memberId, changedNickname);
+        NicknameDto mockDto = NicknameDto.of(1L, "changedNickname");
 
         //when
         Long updatedCount = sut.updateRecipesNicknames(mockDto);
-        String nickname = recipeRepository.findById(memberId).get().getNickname();
 
         //then
-        Assertions.assertThat(nickname).isEqualTo(changedNickname);
-        Assertions.assertThat(updatedCount).isNotNull();
+        String nickname = recipeRepository.findById(mockDto.memberId()).get().getNickname();
+        assertThat(nickname).isEqualTo(mockDto.nickname());
+        assertThat(updatedCount).isNotNull();
+        assertThat(updatedCount).isGreaterThan(0);
     }
 
     @DisplayName("[happy] 유저가 레시피 저장에 성공하면 생성된 레시피의 id가 반환된다.")
@@ -62,18 +69,70 @@ class RecipeAdapterTest extends TotalTestSupport {
         Recipe domain = createRecipeDomain();
 
         //when
-        Long sutRecipe = sut.createRecipe(domain);
+        Long recipeId = sut.createRecipe(domain);
 
         //then
-        Assertions.assertThat(sutRecipe).isEqualTo(10L);
+        assertThat(recipeId).isNotNull();
+        Optional<RecipeEntity> savedRecipe = recipeRepository.findById(recipeId);
+        assertThat(savedRecipe.isPresent()).isTrue();
     }
 
+    @DisplayName("[happy] 새롭게 저장할 레시피의 영양소 정보 저장에 성공하면, 저장된 정보의 ID를 반환한다.")
+    @Test
+    public void createNutritionalInfoSuccess() {
+        // given
+        Recipe domain = createRecipeDomain();
+        Long recipeId = sut.createRecipe(domain);
+
+        // when
+        Long nutritionalInfoId = sut.createNutritionalInfo(domain, recipeId);
+
+        // then
+        assertThat(nutritionalInfoId).isNotNull();
+        Optional<NutritionalInfoEntity> savedNutritionalInfo = nutritionalInfoRepository.findById(nutritionalInfoId);
+        assertThat(savedNutritionalInfo.isPresent()).isTrue();
+    }
+
+
+
     private RecipeEntity createRecipeEntity() {
-        return RecipeEntity.of(1L, "레시피", "레시피 설명", 20, "닭", "#닭발", "{당류: 많음}", "진안", "N");
+        return RecipeEntity.of(
+                1L,
+                "레시피",
+                "레시피 설명",
+                20,
+                "김치, 감자",
+                "#고구마",
+                "진안",
+                "N"
+        );
+    }
+
+    private NutritionalInfoEntity createNutritionalInfoEntity() {
+
+        RecipeEntity recipeEntity = createRecipeEntity();
+        return NutritionalInfoEntity.of(
+                10,
+                10,
+                10,
+                10,
+                10,
+                recipeEntity
+        );
     }
 
     private Recipe createRecipeDomain() {
-        return Recipe.of(10L, 1L, "레시피", "레시피 설명", 20, "닭", "#닭발", "{당류: 많음}", "진안", "N");
+        return Recipe.of(
+                1L,
+                "레시피",
+                "레시피 설명",
+                20,
+                "닭",
+                "#진안",
+                NutritionalInfo.of(10,10,10,10,10),
+                "진안",
+                "N"
+        );
     }
 
 

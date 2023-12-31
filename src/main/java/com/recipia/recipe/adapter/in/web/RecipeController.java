8f5b4ef47c31c528ec5e89260a85e2ad15cf4d6e
wlsdks <wlsdks12@naver.com>
2023-12-31 22:43:19 +0900

    [RCP-254] :sparkles: 어댑터 엔티티 테스트를 진행하며 controller, service, port, adapter 클래스 추가

--- src/main/java/com/recipia/recipe/adapter/in/web/RecipeController.java
+++ src/main/java/com/recipia/recipe/adapter/in/web/RecipeController.java
@@ -1,9 +1,8 @@
 package com.recipia.recipe.adapter.in.web;
 
-import com.recipia.recipe.adapter.in.web.dto.request.RecipeRequestDto;
+import com.recipia.recipe.adapter.in.web.dto.request.RecipeCreateRequestDto;
 import com.recipia.recipe.adapter.in.web.dto.response.ResponseDto;
 import com.recipia.recipe.application.port.in.CreateRecipeUseCase;
-import com.recipia.recipe.application.service.RecipeService;
 import com.recipia.recipe.domain.Recipe;
 import com.recipia.recipe.domain.converter.RecipeConverter;
 import jakarta.validation.Valid;
@@ -26,8 +25,13 @@ public class RecipeController {
      * 유저가 레시피 생성을 요청하는 컨트롤러
      */
     @PostMapping("/createRecipe")
-    public ResponseEntity<ResponseDto<Long>> createRecipe(@Valid @RequestBody RecipeRequestDto recipeRequestDto) {
-        Recipe recipe = RecipeConverter.dtoToDomain(recipeRequestDto);
+    public ResponseEntity<ResponseDto<Long>> createRecipe(@Valid @RequestBody RecipeCreateRequestDto recipeCreateRequestDto) {
+
+
+        // dto를 domain으로 변환
+        Recipe recipe = RecipeConverter.requestDtoToDomain(recipeCreateRequestDto);
+        // domain내부에 jwt 클레임에있는
+
         Long savedRecipeId = createRecipeUseCase.createRecipe(recipe);
 
         return ResponseEntity.ok(ResponseDto.success(savedRecipeId));
--- src/main/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeAdapter.java
+++ src/main/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeAdapter.java
@@ -1,6 +1,7 @@
 package com.recipia.recipe.adapter.out.persistenceAdapter;
 
 import com.recipia.recipe.adapter.out.feign.dto.NicknameDto;
+import com.recipia.recipe.adapter.out.persistence.entity.NutritionalInfoEntity;
 import com.recipia.recipe.adapter.out.persistence.entity.RecipeEntity;
 import com.recipia.recipe.adapter.out.persistenceAdapter.querydsl.RecipeQueryRepository;
 import com.recipia.recipe.application.port.out.RecipePort;
@@ -21,6 +22,7 @@ public class RecipeAdapter implements RecipePort {
 
     private final RecipeQueryRepository querydslRepository;
     private final RecipeRepository recipeRepository;
+    private final NutritionalInfoRepository nutritionalInfoRepository;
 
 
     /**
@@ -39,9 +41,23 @@ public class RecipeAdapter implements RecipePort {
      */
     @Override
     public Long createRecipe(Recipe recipe) {
-        RecipeEntity recipeEntity = RecipeConverter.domainToEntity(recipe);
+        RecipeEntity recipeEntity = RecipeConverter.domainToRecipeEntity(recipe);
         recipeRepository.save(recipeEntity);
         return recipeEntity.getId();
+    }
+
+    /**
+     * 영양소를 저장하는 메서드
+     * 저장에 성공하면 영양소 엔티티의 id값을 반환한다.
+     */
+    @Override
+    public Long createNutritionalInfo(Recipe recipe, Long savedRecipeId) {
+        //1. 도메인에 새롭게 저장하려는 레시피의 id추가
+        recipe.setId(savedRecipeId);
+        //2. 컨버터를 통해 도메인을 영양소 엔티티로 변환
+        NutritionalInfoEntity nutritionalInfoEntity = RecipeConverter.domainToNutritionalInfoEntity(recipe);
+        //3. 저장을 한다.
+        return nutritionalInfoRepository.save(nutritionalInfoEntity).getId();
     }
 
 

4364bb1e536693fba6286ff03e0335681314e696
wlsdks <wlsdks12@naver.com>
2023-12-31 22:40:37 +0900

    [RCP-254] :recycle: 컨버터 코드, 테스트용 data.sql 코드 리팩토링

--- src/main/java/com/recipia/recipe/domain/converter/RecipeConverter.java
+++ src/main/java/com/recipia/recipe/domain/converter/RecipeConverter.java
@@ -1,7 +1,11 @@
 package com.recipia.recipe.domain.converter;
 
-import com.recipia.recipe.adapter.in.web.dto.request.RecipeRequestDto;
+import com.recipia.recipe.adapter.in.web.dto.request.NutritionalInfoDto;
+import com.recipia.recipe.adapter.in.web.dto.request.RecipeCreateRequestDto;
+import com.recipia.recipe.adapter.out.persistence.entity.NutritionalInfoEntity;
 import com.recipia.recipe.adapter.out.persistence.entity.RecipeEntity;
+import com.recipia.recipe.common.utils.SecurityUtil;
+import com.recipia.recipe.domain.NutritionalInfo;
 import com.recipia.recipe.domain.Recipe;
 
 /**
@@ -16,55 +20,80 @@ public class RecipeConverter {
     // entity to domain
     public static Recipe entityToDomain(RecipeEntity entity) {
         return Recipe.of(
-            entity.getId(),
-            entity.getMemberId(),
-            entity.getRecipeName(),
-            entity.getRecipeDesc(),
-            entity.getTimeTaken(),
-            entity.getIngredient(),
-            entity.getHashtag(),
-            entity.getNutritionalInfo(),
-            entity.getNickname(),
-            entity.getDelYn()
+                entity.getId(),
+                entity.getMemberId(),
+                entity.getRecipeName(),
+                entity.getRecipeDesc(),
+                entity.getTimeTaken(),
+                entity.getIngredient(),
+                entity.getHashtag(),
+                null, // fixme: 확인해보기
+                entity.getNickname(),
+                entity.getDelYn()
         );
     }
 
     /**
      * Recipe 도메인을 받아서 RecipeEntity 엔티티로 변환
      */
-    // entity to domain
-    public static RecipeEntity domainToEntity(Recipe domain) {
+    public static RecipeEntity domainToRecipeEntity(Recipe domain) {
         return RecipeEntity.of(
-            domain.getId(),
-            domain.getMemberId(),
-            domain.getRecipeName(),
-            domain.getRecipeDesc(),
-            domain.getTimeTaken(),
-            domain.getIngredient(),
-            domain.getHashtag(),
-            domain.getNutritionalInfo(),
-            domain.getNickname(),
-            domain.getDelYn()
+                domain.getId(),
+                domain.getMemberId(),
+                domain.getRecipeName(),
+                domain.getRecipeDesc(),
+                domain.getTimeTaken(),
+                domain.getIngredient(),
+                domain.getHashtag(),
+                domain.getNickname(),
+                domain.getDelYn()
         );
     }
 
     /**
-     * RequestDto를 도메인으로 반환
+     * Recipe 도메인 내부의 영양소 도메인을 엔티티로 변환하는 로직
+     * 레시피 엔티티에는 저장할때 꼭 필요한 pk값인 id만 필드로 하여 저장해 준다.(최적화)
+     */
+    public static NutritionalInfoEntity domainToNutritionalInfoEntity(Recipe domain) {
+
+        NutritionalInfo nutritionalInfo = domain.getNutritionalInfo();
+        return NutritionalInfoEntity.of(
+                nutritionalInfo.getCarbohydrates(),
+                nutritionalInfo.getProtein(),
+                nutritionalInfo.getFat(),
+                nutritionalInfo.getVitamins(),
+                nutritionalInfo.getMinerals(),
+                RecipeEntity.of(domain.getId())
+        );
+    }
+
+    /**
+     * 레시시 생성을 요청하는 RecipeCreateRequestDto객체를 도메인으로 변환
+     * jwt 클레임으로부터 memberId, nickname을 꺼내서 주입한다.
+     *
      */
     // entity to domain
-    public static Recipe dtoToDomain(RecipeRequestDto dto) {
+    public static Recipe requestDtoToDomain(RecipeCreateRequestDto dto) {
         return Recipe.of(
-                dto.getId(),
-                null,
+                SecurityUtil.getCurrentMemberId(),
                 dto.getRecipeName(),
                 dto.getRecipeDesc(),
                 dto.getTimeTaken(),
                 dto.getIngredient(),
                 dto.getHashtag(),
-                dto.getNutritionalInfo(),
-                null,
-                dto.getDelYn()
+                NutritionalInfo.of(
+                        dto.getNutritionalInfo().getCarbohydrates(),
+                        dto.getNutritionalInfo().getProtein(),
+                        dto.getNutritionalInfo().getFat(),
+                        dto.getNutritionalInfo().getVitamins(),
+                        dto.getNutritionalInfo().getMinerals()
+
+                ),
+                SecurityUtil.getCurrentMemberNickname(),
+                "N" // todo: 하드코딩 맞는지 알아보기
         );
     }
+
+
 
 }

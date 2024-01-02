ccdb4e8b827857f305a7d885ba16297969e59452
wlsdks <wlsdks12@naver.com>
2024-01-02 15:34:30 +0900

    [RCP-254] :white_check_mark: 시큐리티 유틸 클래스 테스트 진행 및 테스트용 jwt설정클래스 추가

--- src/test/java/com/recipia/recipe/common/utils/SecurityUtilTest.java
+++ src/test/java/com/recipia/recipe/common/utils/SecurityUtilTest.java
@@ -1,64 +1,47 @@
 package com.recipia.recipe.common.utils;
 
+import com.recipia.recipe.config.TestJwtConfig;
+import com.recipia.recipe.config.TotalTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-@DisplayName("[단위] 시큐리티 유틸 클래스")
-class SecurityUtilTest {
+@DisplayName("[통합] 시큐리티 유틸 클래스")
+class SecurityUtilTest extends TotalTestSupport {
+
+    @Autowired
+    private SecurityUtil securityUtil;
+
 
     @DisplayName("jwt에서 현재 사용하는 유저의 memberId를 꺼낸다.")
     @Test
     void getCurrentMemberIdTest() {
         //given
-        Jwt jwt = mock(Jwt.class);
-        when(jwt.getClaim("memberId")).thenReturn(12345L);
-
-        Authentication authentication = mock(Authentication.class);
-        when(authentication.isAuthenticated()).thenReturn(true);
-        when(authentication.getPrincipal()).thenReturn(jwt);
-
-        SecurityContext securityContext = mock(SecurityContext.class);
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        // SecurityContextHolder에 모킹한 SecurityContext 설정
-        SecurityContextHolder.setContext(securityContext);
+        Long expectedMemberId = 12345L;
+        String dummyNickname = "userNickname";
+        TestJwtConfig.setupMockJwt(expectedMemberId, dummyNickname);
 
         // when
-        Long memberId = SecurityUtil.getCurrentMemberId();
+        Long memberId = securityUtil.getCurrentMemberId();
 
         // then
         assertEquals(12345L, memberId);
     }
 
-
     @DisplayName("jwt에서 현재 사용하는 유저의 nickname을 꺼낸다.")
     @Test
     void getCurrentMemberNicknameTest() {
         // given
-        Jwt jwt = mock(Jwt.class);
-        when(jwt.getClaim("nickname")).thenReturn("userNickname");
-
-        Authentication authentication = mock(Authentication.class);
-        when(authentication.isAuthenticated()).thenReturn(true);
-        when(authentication.getPrincipal()).thenReturn(jwt);
-
-        SecurityContext securityContext = mock(SecurityContext.class);
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        // SecurityContextHolder에 모킹한 SecurityContext 설정
-        SecurityContextHolder.setContext(securityContext);
+        Long expectedMemberId = 12345L;
+        String dummyNickname = "userNickname";
+        TestJwtConfig.setupMockJwt(expectedMemberId, dummyNickname);
 
         // when
-        String nickname = SecurityUtil.getCurrentMemberNickname();
+        String nickname = securityUtil.getCurrentMemberNickname();
 
         // then
         assertEquals("userNickname", nickname);
     }
-
 }

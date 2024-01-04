11a9b877bd775c904443aa83106a038267b8444c
wlsdks <wlsdks12@naver.com>
2024-01-04 15:31:48 +0900

    [RCP-259] :memo: 검색 컨트롤러 설명 주석, javadoc 추가

--- src/main/java/com/recipia/recipe/adapter/in/search/MongoSearchController.java
+++ src/main/java/com/recipia/recipe/adapter/in/search/MongoSearchController.java
@@ -20,7 +20,11 @@ public class MongoSearchController {
     private final MongoUseCase mongoUseCase;
 
     /**
-     * 사용자가 입력한 단어의 접두사를 기준으로 몽고 db에서 연관된 단어를 검색한다.
+     * 기능: 사용자가 입력한 단어의 "접두사"를 기준으로 mongoDB에서 연관된 단어를 검색한다.
+     * 설명:
+     * 이 컨트롤러는 3가지 상황의 검색 페이지에서 사용되며 dto 내부의 조건(condition)에 따라 검색 결과가 달라진다.
+     * 검색 조건은 3가지로 [전체, 재료, 해시태그] 이다.
+     * 클라이언트는 이 컨트롤러를 호출할 때 검색 받고자 하는 결과의 개수(resultCount)를 담아서 보내줘야 한다. [5 or 10]
      */
     @GetMapping("/search")
     public List<String> search(SearchRequestDto searchRequestDto) {

ad71f2fa861a8d152855e566b780793051ded22b
wlsdks <wlsdks12@naver.com>
2024-01-04 15:06:04 +0900

    [RCP-259] :recycle: 검색 dto에서 클라이언트에게 바로 mongo 필드명을 노출하지 않게 하기위해 구조를 변경하고 valid기능을 추가함

--- src/main/java/com/recipia/recipe/adapter/in/search/dto/SearchRequestDto.java
+++ src/main/java/com/recipia/recipe/adapter/in/search/dto/SearchRequestDto.java
@@ -1,6 +1,8 @@
 package com.recipia.recipe.adapter.in.search.dto;
 
 import com.recipia.recipe.adapter.in.search.constant.SearchType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,22 +14,27 @@ import lombok.NoArgsConstructor;
 @Data
 public class SearchRequestDto {
 
+    @NotBlank
     private SearchType condition;  // 검색조건 (전체, 재료, 해시태그)
+
+    @NotBlank
     private String searchWord; // 사용자가 입력한 검색어
-    private int resultSize;    // 반환될 결과의 최대 개수 (5, 10)
+
+    @NotNull
+    private Integer resultSize;    // 반환될 결과의 최대 개수 (5, 10)
 
     @Builder
-    private SearchRequestDto(SearchType condition, String searchWord, int resultSize) {
+    private SearchRequestDto(SearchType condition, String searchWord, Integer resultSize) {
         this.condition = condition;
         this.searchWord = searchWord;
         this.resultSize = resultSize;
     }
 
-    public static SearchRequestDto of(SearchType condition, String searchWord, int resultSize) {
+    public static SearchRequestDto of(SearchType condition, String searchWord, Integer resultSize) {
         return new SearchRequestDto(condition, searchWord, resultSize);
     }
 
-    public static SearchRequestDto of(String searchWord, int resultSize) {
+    public static SearchRequestDto of(String searchWord, Integer resultSize) {
         return new SearchRequestDto(null, searchWord, resultSize);
     }
 

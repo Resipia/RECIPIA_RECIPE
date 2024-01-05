3aa2c14ee0d5e071d3d52cceb2658bb09a18f1b0
wlsdks <wlsdks12@naver.com>
2024-01-05 14:35:52 +0900

    [RCP-267] :sparkles: 레시피 조회 컨트롤러 코드 작성

--- src/main/java/com/recipia/recipe/adapter/in/web/RecipeController.java
+++ src/main/java/com/recipia/recipe/adapter/in/web/RecipeController.java
@@ -1,17 +1,17 @@
 package com.recipia.recipe.adapter.in.web;
 
 import com.recipia.recipe.adapter.in.web.dto.request.RecipeCreateRequestDto;
+import com.recipia.recipe.adapter.in.web.dto.response.PagingResponseDto;
+import com.recipia.recipe.adapter.in.web.dto.response.RecipeMainListResponseDto;
 import com.recipia.recipe.adapter.in.web.dto.response.ResponseDto;
 import com.recipia.recipe.application.port.in.CreateRecipeUseCase;
+import com.recipia.recipe.application.port.in.ReadRecipeUseCase;
 import com.recipia.recipe.domain.Recipe;
 import com.recipia.recipe.domain.converter.RecipeConverter;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/recipe")
 @RequiredArgsConstructor
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecipeController {
 
     private final CreateRecipeUseCase createRecipeUseCase;
+    private final ReadRecipeUseCase readRecipeUseCase;
     private final RecipeConverter converter;
 
     /**
@@ -33,6 +34,20 @@ public class RecipeController {
         Long savedRecipeId = createRecipeUseCase.createRecipe(recipe);
 
         return ResponseEntity.ok(ResponseDto.success(savedRecipeId));
+    }
+
+    /**
+     * 메인 화면에서 전체 레시피를 조회
+     * page와 size는 각각 '현재 페이지'와 '페이지 당 항목 수'를 의미한다.
+     */
+    @GetMapping("/getAllRecipeList")
+    public ResponseEntity<PagingResponseDto<RecipeMainListResponseDto>> getAllRecipeList(
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sortType", defaultValue = "new") String sortType
+    ) {
+        PagingResponseDto<RecipeMainListResponseDto> allRecipeList = readRecipeUseCase.getAllRecipeList(page, size, sortType);
+        return ResponseEntity.ok(allRecipeList);
     }
 
 }

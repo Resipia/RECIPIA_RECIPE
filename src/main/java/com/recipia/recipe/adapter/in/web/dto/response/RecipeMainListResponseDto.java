aee363cb3b2e98b668f0b804c2135fc0c2a9a042
wlsdks <wlsdks12@naver.com>
2024-01-05 16:00:47 +0900

    [RCP-267] :recycle: 레시피 조회기능 수정으로 응답 dto에 레시피id를 추가하고 서브 카테고리를 리스트로 받도록 변경

--- src/main/java/com/recipia/recipe/adapter/in/web/dto/response/RecipeMainListResponseDto.java
+++ src/main/java/com/recipia/recipe/adapter/in/web/dto/response/RecipeMainListResponseDto.java
@@ -1,20 +1,27 @@
 package com.recipia.recipe.adapter.in.web.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 /**
  * 메인 페이지의 레시피 조회를 할때 데이터를 전달할 객체
  */
-@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class RecipeMainListResponseDto {
     private Long id;
     private String recipeName;
     private String nickname;
-    private String subCategory;
+    private List<String> subCategoryList;
     private boolean isBookmarked;
     //제목, 닉넴, 카테고리(list or string), 북마크 여부, 조회수, order by (조회수 높고낮음, 등록날짜 높고낮음)
 
+    public RecipeMainListResponseDto(Long id, String recipeName, String nickname, boolean isBookmarked) {
+        this.id = id;
+        this.recipeName = recipeName;
+        this.nickname = nickname;
+        this.isBookmarked = isBookmarked;
+    }
 }

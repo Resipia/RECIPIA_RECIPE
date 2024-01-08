64a8133927fd5809c12c3677f908cb5f4d101116
wlsdks <wlsdks12@naver.com>
2024-01-08 14:21:42 +0900

    [RCP-227] :memo: 설명 주석 리팩토링

--- src/main/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeAdapter.java
+++ src/main/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeAdapter.java
@@ -26,8 +26,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
- * Adapter 클래스는 port 인터페이스를 구현한다.
- * port에 요청이 들어가면 port의 메서드를 모두 구현한 이 adapter가 호출되어 동작한다.
+ * 외부(DB)와의 연결을 관리한다.
  */
 @Slf4j
 @RequiredArgsConstructor
@@ -45,7 +44,10 @@ public class RecipeAdapter implements RecipePort {
 
 
     /**
-     * memberId로 유저가 작성한 모든 레시피를 조회한 다음 그 레시피 엔티티가 가진 유저의 닉네임 컬럼을 변경
+     * [UPDATE - 레시피 테이블의 닉네임 컬럼 변경]
+     * MSA 프로젝트라서 레시피 테이블에는 [유저의 닉네임] 컬럼이 존재한다.
+     * 유저가 닉네임을 변경하면 SNS로 이벤트가 발행되고 SQSListener가 동작해서 이 메서드가 호출된다.
+     * 여기서는 memberId로 유저가 작성한 모든 레시피를 조회한 다음 그 레시피 엔티티가 가진 유저의 닉네임 컬럼을 변경한다.
      */
     @Override
     public Long updateRecipesNicknames(NicknameDto nicknameDto) {
@@ -62,7 +64,7 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 레시피를 저장하는 메서드
+     * [CREATE - 레시피 저장]
      * 저장에 성공하면 레시피 엔티티의 id값을 반환한다.
      */
     @Override
@@ -74,7 +76,7 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 영양소를 저장하는 메서드
+     * [CREATE - 영양소 저장]
      * 저장에 성공하면 영양소 엔티티의 id값을 반환한다.
      */
     @Override
@@ -88,8 +90,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 카테고리를 저장하는 메서드
-     * 카테고리 Map 테이블에 저장한다.
+     * [CREATE - 카테고리 저장]
+     * 카테고리 Map 테이블에 서브 카테고리 정보를 저장한다.
      */
     @Override
     public void createRecipeCategoryMap(Recipe recipe, Long savedRecipeId) {
@@ -102,8 +104,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 전체 레시피를 조회하는 메서드
-     * querydsl을 사용해서 데이터를 조회한다.
+     * [READ - 레시피 목록 조회(전체)]
+     * querydsl을 사용해서 데이터를 조회를 최적화 했다. (목록, count)
      */
     @Override
     public Page<RecipeMainListResponseDto> getAllRecipeList(Pageable pageable, String sortType) {
@@ -127,7 +129,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 한개의 레시피를 조회하는 레시피
+     * [READ - 단건의 레시피 상세조회]
+     * 유저가 작성한 레시피 정보를 상세조회한다.
      */
     @Override
     public RecipeDetailViewDto getRecipeDetailView(Long recipeId) {
@@ -144,8 +147,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * s3에 저장된 파일 정보를 rdb에 저장한다.
-     * 서비스 레이어에서 이미 s3에 이미지는 업로드가 완료되고 이곳에서는 그 url정보를 함께 담아서 rdb에 저장한다.
+     * [CREATE - S3에 업로드된 파일(이미지) 정보 저장]
+     * 서비스에서 s3에 이미지 업로드가 완료된 후 호출되어 s3 object의 url정보를 rdb에 저장한다.
      */
     @Override
     public List<Long> saveRecipeFile(List<RecipeFile> recipeFile) {
@@ -169,7 +172,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 레시피를 업데이트 한다.
+     * [UPDATE - 레시피 업데이트]
+     * 레시피의 [이름, 내용(설명), 재료, 해시태그]를 모두 업데이트 한다.
      */
     @Override
     public Long updateRecipe(Recipe recipe) {
@@ -178,7 +182,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 영양소를 업데이트 한다.
+     * [UPDATE - 영양소 업데이트]
+     * 영양소의 모든 필드를 업데이트 한다.
      */
     @Override
     public void updateNutritionalInfo(Recipe recipe) {
@@ -187,8 +192,9 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 카테고리 매핑을 업데이트 한다.
-     * 정책상 최대3개의 카테고리만 업데이트하기 때문에 성능 문제는 없을듯 하여 기존의 카테고리를 지우고 새로운 카테고리를 저장한다.
+     * [UPDATE - 카레고리 매핑정보 업데이트]
+     * 기존의 카테고리를 지우고 새로운 카테고리를 저장한다.
+     * 정책상 사용자는 최대3개의 카테고리만 선택할 수 있기 때문에 성능 문제는 없을듯 하여 선택한 방법이다.
      */
     @Override
     public void updateCategoryMapping(Recipe recipe) {
@@ -206,7 +212,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 레시피와 연관된 파일을 모두 지운다.
+     * [DELETE - 레시피에 연관된 파일 모두 삭제]
+     * S3에 이미지를 업로드할 때 날짜-UUID 이런식으로 저장되어서 RDB의 조건문으로 검색해서 update하기에는 무리가 있어 모두 삭제하고 다시 넣기로 결정
      */
     @Override
     public void deleteRecipeFilesByRecipeId(Long updatedRecipeId) {
@@ -215,8 +222,8 @@ public class RecipeAdapter implements RecipePort {
 
 
     /**
-     * 서브 카테고리를 Map<Long, List<String>> 형태로 받아온다.
-     * Long에는 recipeId가 List<String>에는 서브 카테고리 이름들이 들어가 있다.
+     * [Extract method] - getAllRecipeList, getRecipeDetailView 에서 사용
+     * 서브 카테고리를 맵으로 변환하는 작업 (key: 레시피id, value: 서브 카테고리 리스트(최대3개)
      */
     public Map<Long, List<String>> getSubCategoryNameMap(List<Tuple> subCategoryNameResults) {
         return subCategoryNameResults.stream()
@@ -227,7 +234,8 @@ public class RecipeAdapter implements RecipePort {
     }
 
     /**
-     * 서브 카테고리를 검증한다.
+     * [Extract method] - createRecipeCategoryMap, updateCategoryMapping 에서 사용
+     * 레시피 도메인을 받아와서 서브 카테고리가 null이거나 존재하지 않는 이상한 카테고리인지 검증한다.
      */
     private List<SubCategory> validSubCategory(Recipe recipe) {
         List<SubCategory> subCategoryList = recipe.getSubCategory();
@@ -237,6 +245,8 @@ public class RecipeAdapter implements RecipePort {
             throw new RecipeApplicationException(ErrorCode.CATEGORY_NOT_VALID);
         }
 
+        // 2. 서브 카테고리가 존재하는지 확인한다.
+        // (서브 카테고리는 사용자가 추가하는게 아니라 운영상 고정으로 세팅되어 있기 때문에 이상한 매핑이 되어있지 않은지 확인하는 작업)
         subCategoryList.forEach(subCategory -> {
             // subCategory가 데이터베이스에 존재하는지 확인
             boolean exists = subCategoryEntityRepository.findById(subCategory.getId()).isPresent();

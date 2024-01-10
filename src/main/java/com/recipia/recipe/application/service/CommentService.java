d5b52754b9561e937e3a177ab7228a27d2e15fa1
yijoon009 <yijoon009@gmail.com>
2024-01-10 16:53:16 +0900

    [RCP-282] :recycle: 댓글 서비스 레이어 로직 수정

--- src/main/java/com/recipia/recipe/application/service/CommentService.java
+++ src/main/java/com/recipia/recipe/application/service/CommentService.java
@@ -24,8 +24,8 @@ public class CommentService implements CommentUseCase {
 
     /**
      * [CREATE] 댓글 등록을 담당하는 메서드
-     * 1단계로 댓글을 달려는 레시피가 존재하는지 검증한다.
-     * 존재한다면 댓글을 등록한다.
+     * 1단계 - 댓글을 등록하려는 레시피가 존재하는지 검증한다.
+     * 2단계 - 레시피가 존재한다면 댓글을 등록한다.
      */
     @Transactional
     @Override
@@ -37,9 +37,9 @@ public class CommentService implements CommentUseCase {
 
     /**
      * [UPDATE] 댓글 수정을 담당하는 메서드
-     * 1단계로 댓글을 달려는 레시피가 삭제되지 않은 레시피인지 검증한다. (recipeId, delYn으로 검색)
-     * 2단계로 댓글 수정을 요청한 멤버가 댓글의 원작자인지 검증한다. (commentId, memberId, delYn으로 검색)
-     * 3단계 위 단계를 전부 패스했다면 댓글 내용을 수정한다.
+     * 1단계 - 댓글을 달려는 레시피가 삭제되지 않은 레시피인지 검증한다. (recipeId, delYn으로 검색)
+     * 2단계 - 수정을 요청한 댓글이 삭제되지 않은 댓글인지, 그리고 내가 원작자인지 검증한다. (commentId, memberId, delYn으로 검색)
+     * 3단계 - 위 단계를 전부 패스했다면 댓글 내용을 수정한다.
      */
     @Transactional
     @Override
@@ -47,8 +47,10 @@ public class CommentService implements CommentUseCase {
         // 1단계 - 삭제되지 않은 레시피인지 검증
         checkIsRecipeExist(comment);
 
-        // 내가 작성한 댓글이 맞는지 검증
-        checkIsMyComment(comment);
+        // 2단계 - 삭제된 댓글이 아니고 내가 작성한 댓글이 맞는지 검증
+        checkIsCommentExistAndMine(comment);
+
+        // 3단계 - 댓글 수정
         return commentPort.updateComment(comment);
     }
 
@@ -56,9 +58,9 @@ public class CommentService implements CommentUseCase {
 
     /**
      * [DELETE] 댓글 삭제를 담당하는 메서드
-     * 1단계로 댓글을 달려는 레시피가 삭제되지 않은 레시피인지 검증한다. (recipeId, delYn으로 검색)
-     * 2단계로 댓글 삭제를 요청한 멤버가 댓글의 원작자인지 검증한다. (commentId, memberId, delYn으로 검색)
-     * 3단계 위 단계를 전부 패스했다면 댓글을 삭제한다.
+     * 1단계 - 댓글을 삭제하려는 레시피가 삭제되지 않은 레시피인지 검증한다. (recipeId, delYn으로 검색)
+     * 2단계 - 삭제 요청한 댓글이 삭제되지 않은 댓글인지, 그리고 내가 원작자인지 검증한다. (commentId, memberId, delYn으로 검색)
+     * 3단계 - 위 단계를 전부 패스했다면 댓글을 삭제한다.
      */
     @Transactional
     @Override
@@ -66,8 +68,10 @@ public class CommentService implements CommentUseCase {
         // 1단계 - 삭제되지 않은 레시피인지 검증
         checkIsRecipeExist(comment);
 
-        // 2단계 - 내가 작성한 댓글이 맞는지 검증
-        checkIsMyComment(comment);
+        // 2단계 - 삭제된 댓글이 아니고 내가 작성한 댓글이 맞는지 검증
+        checkIsCommentExistAndMine(comment);
+
+        // 3단계 - 댓글 삭제
         return commentPort.softDeleteComment(comment);
     }
 
@@ -85,11 +89,12 @@ public class CommentService implements CommentUseCase {
     }
 
     /**
-     * [READ] 내가 작성한 댓글이 맞는지 확인하는 메서드
+     * [READ] 삭제된 댓글이 아니고 내가 작성한 댓글이 맞는지 검증
+     * commentId로 댓글이 존재하고, memberId로 내가 원작자인지 검증하는 내부 메서드
      */
-    public boolean checkIsMyComment(Comment comment) {
-        boolean isCommentExist = commentPort.checkIsCommentExist(comment);
-        if(!isCommentExist) {
+    public boolean checkIsCommentExistAndMine(Comment comment) {
+        boolean isCommentExistAndMine = commentPort.checkIsCommentExistAndMine(comment);
+        if(!isCommentExistAndMine) {
             throw new RecipeApplicationException(ErrorCode.COMMENT_IS_NOT_MINE);
         }
         return true;

9588b53333e523972004a69bc766e68957d47acc
wlsdks <wlsdks12@naver.com>
2024-01-11 15:09:07 +0900

    [RCP-252] :sparkles: redis 설정 클래스 작성완료

--- src/main/java/com/recipia/recipe/config/redis/RedisRepositoryConfig.java
+++ src/main/java/com/recipia/recipe/config/redis/RedisRepositoryConfig.java
@@ -1,15 +1,21 @@
 package com.recipia.recipe.config.redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import java.util.Arrays;
+
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
@@ -24,24 +30,31 @@ public class RedisConfig {
     @Value("${spring.redis.database}")
     private int database;
 
+    @Autowired
+    private Environment environment;
+
     // RedisProperties로 yaml에 저장한 host, post, password, database를 연결
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
         redisStandaloneConfiguration.setHostName(host);
         redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
-        redisStandaloneConfiguration.setDatabase(database);
+        Arrays.stream(environment.getActiveProfiles()).forEach(profile -> {
+            if (profile.equals("prod")) {
+                redisStandaloneConfiguration.setPassword(password);
+            }
+        });
+//        redisStandaloneConfiguration.setDatabase(database);
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     // serializer 설정으로 redis-cli를 통해 직접 데이터를 조회할 수 있도록 설정
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+    public RedisTemplate<String, Integer> integerRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Integer> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer()); // 또는 Jackson2JsonRedisSerializer 등
+        template.setValueSerializer(new StringRedisSerializer()); // Integer 값을 저장하기 위한 적절한 serializer 사용
         return template;
     }
 

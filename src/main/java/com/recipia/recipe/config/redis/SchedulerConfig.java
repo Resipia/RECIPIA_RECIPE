470153a492da8e9e2bcd4dc607da481da5bec678
wlsdks <wlsdks12@naver.com>
2024-01-11 15:14:29 +0900

    [RCP-252] :sparkles: 스케쥴러 설정 코드 추가

---
+++ src/main/java/com/recipia/recipe/config/redis/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package com.recipia.recipe.config.redis;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {
+}

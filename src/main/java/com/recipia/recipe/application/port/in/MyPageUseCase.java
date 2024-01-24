679b81f4a5a6efabcf2941b2e55dae88c4918be6
yijoon009 <yijoon009@gmail.com>
2024-01-24 21:45:34 +0900

    [RCP-356] :sparkles: 내가 작성한 레시피 목록 가져오기 (service 레이어)

--- src/main/java/com/recipia/recipe/application/port/in/MyPageUseCase.java
+++ src/main/java/com/recipia/recipe/application/port/in/MyPageUseCase.java
@@ -1,5 +1,6 @@
 package com.recipia.recipe.application.port.in;
 
+import com.recipia.recipe.adapter.in.web.dto.response.PagingResponseDto;
 import com.recipia.recipe.adapter.in.web.dto.response.RecipeMainListResponseDto;
 import com.recipia.recipe.domain.MyPage;
 
@@ -8,4 +9,5 @@ import java.util.List;
 public interface MyPageUseCase {
     MyPage getRecipeCount(Long memberId);
     List<RecipeMainListResponseDto> getMyRecipeHigh(Long memberId);
+    PagingResponseDto<RecipeMainListResponseDto> getAllMyRecipeList(int page, int size, String sortType);
 }

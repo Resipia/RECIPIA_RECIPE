fe0a377c5072ef82bfc860af9a095220962ae75b
yijoon009 <yijoon009@gmail.com>
2024-01-25 15:15:03 +0900

    [RCP-358] :sparkles: 1. 내가 좋아요한 레시피 목록 가져오기 (service) 2. 썸네일 preUrl 설정해주는 메서드 내부 메서드로 추출

--- src/main/java/com/recipia/recipe/application/service/MyPageService.java
+++ src/main/java/com/recipia/recipe/application/service/MyPageService.java
@@ -48,27 +48,21 @@ public class MyPageService implements MyPageUseCase {
         List<Long> recipeIds = recipePort.getTargetMemberRecipeIds(targetMemberId);
 
         // targetMember가 작성한 레시피가 존재하지 않는다면 그냥 return null
-        if(recipeIds.isEmpty()) {
+        if (recipeIds.isEmpty()) {
             return null;
         }
 
         // targetMember가 작성한 레시피가 존재한다면 redis 에서 조회수 높은순으로 가져오기
         List<Long> highRecipeIds = getTargetMemberHighRecipeIds(recipeIds);
+
+        // 레시피 조회수 top5에 해당하는 레시피 목록 가져오기
         List<RecipeListResponseDto> databaseResult = recipePort.getTargetMemberHighRecipeList(targetMemberId, highRecipeIds);
 
-        List<RecipeListResponseDto> finalResult = databaseResult.stream().map(dto -> {
-            // 만약 저장된 썸네일이 있다면, S3 pre-signed-url을 생성한다.
-            if (dto.getThumbnailFullPath() != null) {
-                String preSignedUrl = imageS3Service.generatePreSignedUrl(dto.getThumbnailFullPath(), 60);
-                // 생성된 pre-signed-url을 세팅해주고 반환한다.
-                return RecipeListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
-            }
-            // 만약 저장된 썸네일이 없다면 기존 데이터를 반환한다.
-            return dto;
-        }).collect(Collectors.toList());
-
+        // 가져온 레시피중에서 썸네일이 존재하는 레시피가 있다면 preUrl을 세팅해준다.
+        List<RecipeListResponseDto> finalResult = setPreUrls(databaseResult);
         return finalResult;
     }
+
 
     /**
      * [READ] targetMember가 작성한 모든 레시피 목록을 가져온다
@@ -83,19 +77,9 @@ public class MyPageService implements MyPageUseCase {
 
         // 받아온 데이터를 꺼내서 응답 dto에 값을 세팅해 준다.
         List<RecipeListResponseDto> beforeContent = databaseResult.getContent();
-
-        List<RecipeListResponseDto> finalResult = beforeContent.stream().map(dto -> {
-            // 만약 저장된 썸네일이 있다면, S3 pre-signed-url을 생성한다.
-            if (dto.getThumbnailFullPath() != null) {
-                String preSignedUrl = imageS3Service.generatePreSignedUrl(dto.getThumbnailFullPath(), 60);
-                // 생성된 pre-signed-url을 세팅해주고 반환한다.
-                return RecipeListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
-            }
-            // 만약 저장된 썸네일이 없다면 기존 데이터를 반환한다.
-            return dto;
-        }).collect(Collectors.toList());
-
-
+        // 썸네일 데이터를 preUrl로 세팅해준다.
+        List<RecipeListResponseDto> finalResult = setPreUrls(beforeContent);
+        // 총 갯수를 세팅해준다.
         Long totalCount = databaseResult.getTotalElements();
         return PagingResponseDto.of(finalResult, totalCount);
     }
@@ -110,27 +94,37 @@ public class MyPageService implements MyPageUseCase {
 
         // 데이터를 받아온다.
         Page<RecipeListResponseDto> databaseResult = recipePort.getAllMyBookmarkList(pageable);
-
         // 받아온 데이터를 꺼내서 응답 dto에 값을 세팅해 준다.
         List<RecipeListResponseDto> beforeContent = databaseResult.getContent();
-
-        List<RecipeListResponseDto> finalResult = beforeContent.stream().map(dto -> {
-            // 만약 저장된 썸네일이 있다면, S3 pre-signed-url을 생성한다.
-            if (dto.getThumbnailFullPath() != null) {
-                String preSignedUrl = imageS3Service.generatePreSignedUrl(dto.getThumbnailFullPath(), 60);
-                // 생성된 pre-signed-url을 세팅해주고 반환한다.
-                return RecipeListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
-            }
-            // 만약 저장된 썸네일이 없다면 기존 데이터를 반환한다.
-            return dto;
-        }).collect(Collectors.toList());
-
-
+        // 썸네일 데이터를 preUrl로 세팅해준다.
+        List<RecipeListResponseDto> finalResult = setPreUrls(beforeContent);
+        // 총 갯수를 세팅해준다.
         Long totalCount = databaseResult.getTotalElements();
         return PagingResponseDto.of(finalResult, totalCount);
     }
 
     /**
+     * [READ] 내가 좋아요한 레시피 목록을 가져온다.
+     */
+    @Override
+    public PagingResponseDto<RecipeListResponseDto> getAllMyLikeList(int page, int size) {
+        // Pageable 객체 생성
+        Pageable pageable = PageRequest.of(page, size);
+
+        // 데이터를 받아온다.
+        Page<RecipeListResponseDto> databaseResult = recipePort.getAllMyLikeList(pageable);
+
+        // 받아온 데이터를 꺼내서 응답 dto에 값을 세팅해 준다.
+        List<RecipeListResponseDto> beforeContent = databaseResult.getContent();
+        // 썸네일 데이터를 preUrl로 세팅해준다.
+        List<RecipeListResponseDto> finalResult = setPreUrls(beforeContent);
+        // 총 갯수를 세팅해준다.
+        Long totalCount = databaseResult.getTotalElements();
+        return PagingResponseDto.of(finalResult, totalCount);
+    }
+
+    /**
+     * [Extract Method]
      * redis에서 파라미터로 받은 id에 해당하는 데이터중 조회수 높은순으로 5개 가져오기
      */
     private List<Long> getTargetMemberHighRecipeIds(List<Long> myRecipeIds) {
@@ -148,5 +142,22 @@ public class MyPageService implements MyPageUseCase {
                 .limit(5)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * [Extract Method]
+     * DB에서 응답받은 데이터중에서 썸네일이 있는 경우, pre-signed-url을 생성해서 세팅한다.
+     */
+    private List<RecipeListResponseDto> setPreUrls(List<RecipeListResponseDto> databaseResult) {
+        return databaseResult.stream().map(dto -> {
+            // 만약 저장된 썸네일이 있다면, S3 pre-signed-url을 생성한다.
+            if (dto.getThumbnailFullPath() != null) {
+                String preSignedUrl = imageS3Service.generatePreSignedUrl(dto.getThumbnailFullPath(), 60);
+                // 생성된 pre-signed-url을 세팅해주고 반환한다.
+                return RecipeListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
+            }
+            // 만약 저장된 썸네일이 없다면 기존 데이터를 반환한다.
+            return dto;
+        }).collect(Collectors.toList());
     }
 }

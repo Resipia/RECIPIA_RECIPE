56891dc3802887442dac5a0d74af7a6ff721bbfa
yijoon009 <yijoon009@gmail.com>
2024-01-25 17:10:55 +0900

    [RCP-363] :sparkles: 레시피 삭제시 레시피 좋아요 삭제 기능 추가

--- src/main/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeLikeRepository.java
+++ src/main/java/com/recipia/recipe/adapter/out/persistenceAdapter/RecipeLikeRepository.java
@@ -4,9 +4,12 @@ import com.recipia.recipe.adapter.out.persistence.entity.RecipeLikeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeLikeRepository extends JpaRepository<RecipeLikeEntity, Long> {
 
     boolean existsByIdAndMemberId(Long id, Long memberId);
+    List<RecipeLikeEntity> findAllByRecipeEntity_Id(Long recipeId);
 
 }

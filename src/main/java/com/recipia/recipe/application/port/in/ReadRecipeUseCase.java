2bf573046dcd7790cc8df9303949ccd336cffae5
yijoon009 <yijoon009@gmail.com>
2024-01-25 14:00:58 +0900

    [RCP-357] :sparkles: 1. 내가 북마크한 레시피 목록 가져오기 2. 작성한 레시피, 작성레시피 목록 targetId로 수정 (service)

--- src/main/java/com/recipia/recipe/application/port/in/ReadRecipeUseCase.java
+++ src/main/java/com/recipia/recipe/application/port/in/ReadRecipeUseCase.java
@@ -1,14 +1,14 @@
 package com.recipia.recipe.application.port.in;
 
 import com.recipia.recipe.adapter.in.web.dto.response.PagingResponseDto;
-import com.recipia.recipe.adapter.in.web.dto.response.RecipeMainListResponseDto;
+import com.recipia.recipe.adapter.in.web.dto.response.RecipeListResponseDto;
 import com.recipia.recipe.domain.Recipe;
 
 import java.util.List;
 
 public interface ReadRecipeUseCase {
 
-    PagingResponseDto<RecipeMainListResponseDto> getAllRecipeList(int page, int size, String sortType, List<Long> subCategoryList);
+    PagingResponseDto<RecipeListResponseDto> getAllRecipeList(int page, int size, String sortType, List<Long> subCategoryList);
 
     Recipe getRecipeDetailView(Recipe domain);
 }
--- src/main/java/com/recipia/recipe/application/service/MyPageService.java
+++ src/main/java/com/recipia/recipe/application/service/MyPageService.java
@@ -1,7 +1,7 @@
 package com.recipia.recipe.application.service;
 
 import com.recipia.recipe.adapter.in.web.dto.response.PagingResponseDto;
-import com.recipia.recipe.adapter.in.web.dto.response.RecipeMainListResponseDto;
+import com.recipia.recipe.adapter.in.web.dto.response.RecipeListResponseDto;
 import com.recipia.recipe.application.port.in.MyPageUseCase;
 import com.recipia.recipe.application.port.out.RecipePort;
 import com.recipia.recipe.application.port.out.RedisPort;
@@ -31,35 +31,37 @@ public class MyPageService implements MyPageUseCase {
     private final ImageS3Service imageS3Service;
 
     /**
-     * [READ] memberId에 해당하는 회원이 작성한 레시피 갯수를 가져온다.
+     * [READ] targetMemberId에 해당하는 회원이 작성한 레시피 갯수를 가져온다.
      */
     @Override
-    public MyPage getRecipeCount(Long memberId) {
-        Long myRecipeCount = recipePort.getMyRecipeCount(memberId);
-        return MyPage.of(myRecipeCount);
+    public MyPage getRecipeCount(Long targetMemberId) {
+        Long recipeCount = recipePort.getTargetMemberIdRecipeCount(targetMemberId);
+        return MyPage.of(recipeCount);
     }
 
     /**
-     * [READ] 내가 작성한 레시피 목록을 조회수 높은 순으로 최대 5개 가져온다.
+     * [READ] targetMemberId가 작성한 레시피 목록을 조회수 높은 순으로 최대 5개 가져온다.
      */
     @Override
-    public List<RecipeMainListResponseDto> getMyRecipeHigh(Long memberId) {
+    public List<RecipeListResponseDto> getTargetMemberRecipeHigh(Long targetMemberId) {
 
-        List<Long> myRecipeIds = recipePort.getAllMyRecipeIds(memberId);
+        List<Long> recipeIds = recipePort.getTargetMemberRecipeIds(targetMemberId);
 
-        // 내가 작성한 레시피가 존재한다면 redis 에서 조회수 높은순으로 가져오기
-        if(myRecipeIds.isEmpty()) {
+        // targetMember가 작성한 레시피가 존재하지 않는다면 그냥 return null
+        if(recipeIds.isEmpty()) {
             return null;
         }
-        List<Long> myHighRecipeIds = getMyHighRecipeIds(myRecipeIds);
-        List<RecipeMainListResponseDto> databaseResult = recipePort.getMyHighRecipeList(memberId, myHighRecipeIds);
 
-        List<RecipeMainListResponseDto> finalResult = databaseResult.stream().map(dto -> {
+        // targetMember가 작성한 레시피가 존재한다면 redis 에서 조회수 높은순으로 가져오기
+        List<Long> highRecipeIds = getTargetMemberHighRecipeIds(recipeIds);
+        List<RecipeListResponseDto> databaseResult = recipePort.getTargetMemberHighRecipeList(targetMemberId, highRecipeIds);
+
+        List<RecipeListResponseDto> finalResult = databaseResult.stream().map(dto -> {
             // 만약 저장된 썸네일이 있다면, S3 pre-signed-url을 생성한다.
             if (dto.getThumbnailFullPath() != null) {
                 String preSignedUrl = imageS3Service.generatePreSignedUrl(dto.getThumbnailFullPath(), 60);
                 // 생성된 pre-signed-url을 세팅해주고 반환한다.
-                return RecipeMainListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
+                return RecipeListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
             }
             // 만약 저장된 썸네일이 없다면 기존 데이터를 반환한다.
             return dto;
@@ -69,25 +71,55 @@ public class MyPageService implements MyPageUseCase {
     }
 
     /**
-     * [READ] 내가 작성한 모든 레시피 목록을 가져온다
+     * [READ] targetMember가 작성한 모든 레시피 목록을 가져온다
      */
     @Override
-    public PagingResponseDto<RecipeMainListResponseDto> getAllMyRecipeList(int page, int size, String sortType) {
+    public PagingResponseDto<RecipeListResponseDto> getTargetMemberRecipeList(int page, int size, String sortType, Long targetMemberId) {
         // Pageable 객체 생성
         Pageable pageable = PageRequest.of(page, size);
 
         // 데이터를 받아온다.
-        Page<RecipeMainListResponseDto> databaseResult = recipePort.getAllMyRecipeList(pageable, sortType);
+        Page<RecipeListResponseDto> databaseResult = recipePort.getTargetMemberRecipeList(targetMemberId, pageable, sortType);
 
         // 받아온 데이터를 꺼내서 응답 dto에 값을 세팅해 준다.
-        List<RecipeMainListResponseDto> beforeContent = databaseResult.getContent();
+        List<RecipeListResponseDto> beforeContent = databaseResult.getContent();
 
-        List<RecipeMainListResponseDto> finalResult = beforeContent.stream().map(dto -> {
+        List<RecipeListResponseDto> finalResult = beforeContent.stream().map(dto -> {
             // 만약 저장된 썸네일이 있다면, S3 pre-signed-url을 생성한다.
             if (dto.getThumbnailFullPath() != null) {
                 String preSignedUrl = imageS3Service.generatePreSignedUrl(dto.getThumbnailFullPath(), 60);
                 // 생성된 pre-signed-url을 세팅해주고 반환한다.
-                return RecipeMainListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
+                return RecipeListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
+            }
+            // 만약 저장된 썸네일이 없다면 기존 데이터를 반환한다.
+            return dto;
+        }).collect(Collectors.toList());
+
+
+        Long totalCount = databaseResult.getTotalElements();
+        return PagingResponseDto.of(finalResult, totalCount);
+    }
+
+    /**
+     * [READ] 내가 북마크한 레시피 목록을 가져온다.
+     */
+    @Override
+    public PagingResponseDto<RecipeListResponseDto> getAllMyBookmarkList(int page, int size) {
+        // Pageable 객체 생성
+        Pageable pageable = PageRequest.of(page, size);
+
+        // 데이터를 받아온다.
+        Page<RecipeListResponseDto> databaseResult = recipePort.getAllMyBookmarkList(pageable);
+
+        // 받아온 데이터를 꺼내서 응답 dto에 값을 세팅해 준다.
+        List<RecipeListResponseDto> beforeContent = databaseResult.getContent();
+
+        List<RecipeListResponseDto> finalResult = beforeContent.stream().map(dto -> {
+            // 만약 저장된 썸네일이 있다면, S3 pre-signed-url을 생성한다.
+            if (dto.getThumbnailFullPath() != null) {
+                String preSignedUrl = imageS3Service.generatePreSignedUrl(dto.getThumbnailFullPath(), 60);
+                // 생성된 pre-signed-url을 세팅해주고 반환한다.
+                return RecipeListResponseDto.of(dto.getId(), dto.getRecipeName(), dto.getNickname(), dto.getBookmarkId(), dto.getSubCategoryList(), null, preSignedUrl);
             }
             // 만약 저장된 썸네일이 없다면 기존 데이터를 반환한다.
             return dto;
@@ -101,7 +133,7 @@ public class MyPageService implements MyPageUseCase {
     /**
      * redis에서 파라미터로 받은 id에 해당하는 데이터중 조회수 높은순으로 5개 가져오기
      */
-    private List<Long> getMyHighRecipeIds(List<Long> myRecipeIds) {
+    private List<Long> getTargetMemberHighRecipeIds(List<Long> myRecipeIds) {
         Map<Long, Integer> recipeViews = new HashMap<>();
 
         // 각 레시피 ID에 대한 조회수를 가져와 매핑
